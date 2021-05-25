a86e332bffdae5e5a10c148e39b53084a9aa0cdc
JosDeleon <35647096+JosDeleon@users.noreply.github.com>
2021-05-25 12:42:01 -0600

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -3,6 +3,24 @@ const dotenv = require('dotenv');
 const app = new express();
 dotenv.config();
 
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstardingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstarding = new NaturalLanguageUnderstardingV1 ({
+        version : '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+
+    return naturalLanguageUnderstarding;
+}
+
 NLU = getNLUInstance();
 
 app.use(express.static('client'));
@@ -97,21 +115,3 @@ app.get("/text/sentiment", (req,res) => {
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
-function getNLUInstance(){
-    let api_key = process.env.API_KEY;
-    let api_url = process.env.API_URL;
-
-    const NaturalLanguageUnderstardingV1 = require('ibm-watson/natural-language-understanding/v1');
-    const { IamAuthenticator } = require('ibm-watson/auth');
-
-    const naturalLanguageUnderstarding = new NaturalLanguageUnderstardingV1 ({
-        version : '2020-08-01',
-        authenticator: new IamAuthenticator({
-            apikey: api_key,
-        }),
-        serviceUrl: api_url,
-    });
-
-    return naturalLanguageUnderstarding;
-}
